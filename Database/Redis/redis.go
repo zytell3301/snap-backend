e242c5200dd727364f3b40c203eeb37e2c716370
zytell3301 <zytell3301@gmail.com>
2021-09-03 01:36:55 +0430

    Implement redis connection intitation

--- Database/Redis/redis.go
+++ Database/Redis/redis.go
@@ -1,5 +1,30 @@
 package Redis
 
-import "github.com/spf13/viper"
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/viper"
+)
 
 var Configs *viper.Viper
+var Connection *redis.Client
+
+func InitiateRedis() {
+	connection := redis.NewClient(&redis.Options{
+		Addr:      Configs.GetString("host") + ":" + Configs.GetString("port"),
+		OnConnect: onConnect,
+	})
+
+	Connection = connection
+}
+
+func onConnect(_ context.Context, cn *redis.Conn) error {
+	status := cn.Ping(context.Background())
+	result, _ := status.Result()
+
+	switch result != "PONG" {
+	case true:
+		panic("Failed to ping redis server")
+	}
+	return nil
+}
