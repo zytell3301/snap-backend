f7a0223c1ae5def11644310565f97a1b737fb698
zytell3301 <zytell3301@gmail.com>
2021-09-05 19:57:27 +0430

    Proceed grpc server initation

---
+++ DriversService/startup.go
@@ -0,0 +1,31 @@
+package DriversService
+
+import (
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"net"
+	"snap/DriversService/DriversLocationService"
+	"snap/DriversService/GrpcServices"
+)
+
+var Configs *viper.Viper
+
+func InitiateGrpcServices() {
+	listener, err := net.Listen("tcp", Configs.GetString("listen_address")+":"+Configs.GetString("port"))
+
+	switch err != nil {
+	case true:
+		panic(err)
+	}
+
+	server := grpc.NewServer()
+
+	GrpcServices.RegisterDriversLocationReportServer(server, &DriversLocationService.DriversLocationService{})
+
+	err = server.Serve(listener)
+
+	switch err != nil {
+	case true:
+		panic(err)
+	}
+}
