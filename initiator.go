c67b0aa8381f737ab4ab664efcc1724f20e4b598
zytell3301 <zytell3301@gmail.com>
2021-09-06 19:21:07 +0430

    Initiate cassandra package

--- initiator.go
+++ initiator.go
@@ -4,10 +4,12 @@ import (
 	"snap/Database/Cassandra/Keyspaces"
 	"snap/Database/Redis"
 	"snap/Database/Uuid"
+	"snap/DriversService"
 )
 
 func initiatePackages() {
 	Redis.InitiateRedis()
 	Uuid.InitSpace()
 	Keyspaces.InitiateKeyspaces()
+	go DriversService.InitiateGrpcServices()
 }
