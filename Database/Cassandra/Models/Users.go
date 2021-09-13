acda434595d902d26ec159f5c0a31c3092238261
zytell3301 <zytell3301@gmail.com>
2021-09-13 17:18:53 +0430

    Define a separate type for Users table and corresponding udt

--- Database/Cassandra/Models/Users.go
+++ Database/Cassandra/Models/Users.go
@@ -1,16 +1,39 @@
 package Models
 
-import "snap/Database/Cassandra"
+import (
+	"github.com/gocql/gocql"
+	"snap/Database/Cassandra"
+	"time"
+)
 
-var Users = Cassandra.TableMetaData{
-	Table: "users",
-	Columns: map[string]struct{}{
-		"id":             {},
-		"phone":          {},
-		"balance":        {},
-		"driver_details": {},
-		"created_at":     {},
+type Driver struct {
+	name        string
+	lastname    string
+	vehicle_no  string
+	balance     int
+	profile_pic string
+}
+
+type User struct {
+	Cassandra.TableMetaData
+	id             gocql.UUID
+	phone          string
+	balance        int
+	driver_details Driver
+	created_at     time.Time
+}
+
+var Users = User{
+	TableMetaData: Cassandra.TableMetaData{
+		Table: "users",
+		Columns: map[string]struct{}{
+			"id":             {},
+			"phone":          {},
+			"balance":        {},
+			"driver_details": {},
+			"created_at":     {},
+		},
+		Pk:       map[string]struct{}{"id": {}},
+		Keyspace: "snap",
 	},
-	Pk:         map[string]struct{}{"id": {}},
-	Keyspace:   "snap",
 }
