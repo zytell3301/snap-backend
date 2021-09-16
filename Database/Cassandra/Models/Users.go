46f2a6aba6a8c3a9b23dbd9f37b2a7efb114172d
zytell3301 <zytell3301@gmail.com>
2021-09-17 01:42:02 +0430

    Export User type fields and their corresponding refrences

--- Database/Cassandra/Models/Users.go
+++ Database/Cassandra/Models/Users.go
@@ -10,37 +10,37 @@ type Driver struct {
 	Name        string `cql:"name"`
 	Lastname    string `cql:"lastname"`
 	Vehicle_no  string `cql:"vehicle_no"`
-	Balance     int    `cql:"balance"`
+	Balance     int    `cql:"Balance"`
 	Profile_pic string `cql:"profile_pic"`
 }
 
 type User struct {
 	Cassandra.TableMetaData
-	id             gocql.UUID
-	phone          string
-	balance        int
-	driver_details Driver
-	created_at     time.Time
+	Id             gocql.UUID
+	Phone          string
+	Balance        int
+	Driver_details Driver
+	Created_at     time.Time
 }
 
 var Users = User{
 	TableMetaData: Cassandra.TableMetaData{
 		Table: "users",
 		Columns: map[string]struct{}{
-			"id":             {},
-			"phone":          {},
-			"balance":        {},
-			"driver_details": {},
-			"created_at":     {},
+			"Id":             {},
+			"Phone":          {},
+			"Balance":        {},
+			"Driver_details": {},
+			"Created_at":     {},
 		},
-		Pk:       map[string]struct{}{"id": {}},
+		Pk:       map[string]struct{}{"Id": {}},
 		Keyspace: "snap",
 	},
 }
 
 func (u User) GetDriverDetails(conditions map[string]interface{}) (user User,err error) {
-	statement := u.GetSelectStatement(conditions, []string{"id","driver_details"})
-	err = statement.Scan(&user.id,&user.driver_details)
+	statement := u.GetSelectStatement(conditions, []string{"Id","Driver_details"})
+	err = statement.Scan(&user.Id,&user.Driver_details)
 
 	return
 }
