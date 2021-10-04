c14d2752a7e6daa64675e0fe609572be11361ef1
zytell3301 <zytell3301@gmail.com>
2021-10-04 09:13:45 +0330

    Set user_pk_phone model connection after startup

--- Database/Cassandra/Models/startup.go
+++ Database/Cassandra/Models/startup.go
@@ -5,4 +5,5 @@ import "snap/Database/Cassandra"
 func EvaluateCassandraModelsSession() {
 	Tokens.Connection = Cassandra.ConnectionManager.GetSession(Tokens.Keyspace)
 	Users.Connection = Cassandra.ConnectionManager.GetSession(Users.Keyspace)
+	UserPkPhone.Connection = Cassandra.ConnectionManager.GetSession(UserPkPhone.Keyspace)
 }
