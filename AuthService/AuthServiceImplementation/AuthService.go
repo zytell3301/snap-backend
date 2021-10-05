241fe4275d50c2a5a81018274915d6a4d73836ed
zytell3301 <zytell3301@gmail.com>
2021-10-05 11:00:30 +0330

    Implement NewUser grpc method in AuthService

--- AuthService/AuthServiceImplementation/AuthService.go
+++ AuthService/AuthServiceImplementation/AuthService.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/gocql/gocql"
 	"golang.org/x/crypto/bcrypt"
 	"snap/AuthService/GrpcServices"
 	"snap/Database/Cassandra/Models"
@@ -35,6 +36,51 @@ func (AuthServiceImplementation) Authenticate(ctx context.Context, credentials *
 	switch token == "" {
 	case true:
 		return &GrpcServices.Token{}, errors.New("an error occurred while generating token. please try again later")
+	}
+
+	return &GrpcServices.Token{Token: token}, nil
+}
+
+func (AuthServiceImplementation) NewAccount(ctx context.Context, user *GrpcServices.User) (*GrpcServices.Token, error) {
+	usr := Models.UserPkPhone.GetUser(user.Phone)
+
+	switch usr.Phone != "" {
+	case true:
+		return &GrpcServices.Token{}, errors.New("user already exists")
+	}
+
+	userId, err := gocql.RandomUUID()
+	switch err != nil {
+	case true:
+		fmt.Println(err)
+		return &GrpcServices.Token{}, errors.New("an error occurred while creating a new user. please try again later")
+	}
+
+	driverDetails := Models.Driver{
+		Name:       user.DriverDetails.Name,
+		Lastname:   user.DriverDetails.Lastname,
+		Vehicle_no: user.DriverDetails.VehicleNo,
+		Balance:    0,
+	}
+
+	err = Models.Users.NewUser(Models.User{
+		Id:             userId,
+		Phone:          user.Phone,
+		Password:       user.Password,
+		Driver_details: driverDetails,
+	})
+
+	switch err != nil {
+	case true:
+		fmt.Println(err)
+		return &GrpcServices.Token{}, errors.New("an error occurred while creating a new user. please try again later")
+	}
+
+	token := Models.Tokens.GenerateToken(userId)
+
+	switch token == "" {
+	case true:
+		return &GrpcServices.Token{}, errors.New("an error occurred while generating token. please attempt to login again")
 	}
 
 	return &GrpcServices.Token{Token: token}, nil
